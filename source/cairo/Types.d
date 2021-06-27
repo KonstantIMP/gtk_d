c97c80c9fd4cf13d9872de1230f3b0deb2638f8c
KonstantIMP <mihedovkos@gmail.com>
2021-06-27 18:24:55 +0300

    Add cairo

---
+++ source/cairo/Types.d
@@ -0,0 +1,43 @@
+
+
+/*
+ * Conversion parameters:
+ * inFile  = cairo-Types.html
+ * outPack = cairo
+ * outFile = Types
+ * strct   = 
+ * realStrct=
+ * ctorStrct=
+ * clss    = 
+ * interf  = 
+ * class Code: No
+ * interface Code: No
+ * template for:
+ * extend  = 
+ * implements:
+ * prefixes:
+ * omit structs:
+ * omit prefixes:
+ * 	- cairo_bool_t
+ * omit code:
+ * omit signals:
+ * imports:
+ * structWrap:
+ * module aliases:
+ * local aliases:
+ * overrides:
+ */
+
+module cairo.Types;
+
+public  import cairo.c.types;
+
+private import cairo.c.functions;
+private import glib.ConstructionException;
+
+
+
+
+/**
+ */
+
